d712df723219dd6046b9960052580011861c1263
KrivoKu <vovka2002108@gmail.com>
2021-06-02 00:26:47 +0300

    Minimum UI added

--- ReportGeneratorPlugin.UI/ReportGeneratorPluginUIPackage.cs
+++ ReportGeneratorPlugin.UI/ReportGeneratorPluginUIPackage.cs
@@ -24,30 +24,32 @@ namespace ReportGeneratorPlugin.UI
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [Guid(ReportGeneratorPlugin.UIPackage.PackageGuidString)]
-    public sealed class ReportGeneratorPlugin.UIPackage : AsyncPackage
+    [Guid(ReportGeneratorPluginUIPackage.PackageGuidString)]
+    public sealed class ReportGeneratorPluginUIPackage : AsyncPackage
     {
-         /// <summary>
+        /// <summary>
         /// ReportGeneratorPlugin.UIPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "67d8cad4-68cc-4bd6-a942-9c34751dd022";
 
-    #region Package Members
 
-    /// <summary>
-    /// Initialization of the package; this method is called right after the package is sited, so this is the place
-    /// where you can put all the initialization code that rely on services provided by VisualStudio.
-    /// </summary>
-    /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-    /// <param name="progress">A provider for progress updates.</param>
-    /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-    {
-        // When initialized asynchronously, the current thread may be a background thread at this point.
-        // Do any initialization that requires the UI thread after switching to the UI thread.
-        await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+
+        #endregion
     }
+}
 
-    #endregion
-}
-}
