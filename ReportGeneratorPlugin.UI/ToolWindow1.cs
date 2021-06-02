43ca54338be46ba6095017b643c0ed4531e28731
KrivoKu <vovka2002108@gmail.com>
2021-06-03 01:26:05 +0300

    Fix

--- ReportGeneratorPlugin.UI/ToolWindow1.cs
+++ ReportGeneratorPlugin.UI/ToolWindow1.cs
@@ -23,7 +23,7 @@ namespace ReportGeneratorPlugin.UI
         /// </summary>
         public ToolWindow1() : base(null)
         {
-            Caption = "ToolWindow1";
+            Caption = "ReportGenerator";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
